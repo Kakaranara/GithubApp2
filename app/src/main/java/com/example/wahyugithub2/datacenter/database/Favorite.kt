eed8fad099cde198a2fd5481672ef5991dfb6636
Wahyu <wahyukoco535@gmail.com>
2022-09-06 19:00:47 +0700

    feat : adding FAB in xml and change db structure (not yet completed)

--- app/src/main/java/com/example/wahyugithub2/datacenter/database/Favorite.kt
+++ app/src/main/java/com/example/wahyugithub2/datacenter/database/Favorite.kt
@@ -1,10 +1,15 @@
 package com.example.wahyugithub2.datacenter.database
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.wahyugithub2.datacenter.pojo.DetailUserResponse
 
 @Entity
 data class Favorite(
-    @PrimaryKey
-    val id : Int = 0
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+
+    @ColumnInfo(name = "data")
+    val data : String? = null
 )
--- app/src/main/java/com/example/wahyugithub2/datacenter/database/FavoriteDao.kt
+++ app/src/main/java/com/example/wahyugithub2/datacenter/database/FavoriteDao.kt
@@ -1,9 +1,19 @@
 package com.example.wahyugithub2.datacenter.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
+import com.example.wahyugithub2.datacenter.pojo.DetailUserResponse
 
 
 @Dao
 interface FavoriteDao {
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(favorite: Favorite)
+
+    @Delete
+    fun delete(favorite: Favorite)
+
+    @Query("SELECT * FROM favorite ORDER BY id ASC")
+    fun getAllData() : LiveData<List<Favorite>>
 }
